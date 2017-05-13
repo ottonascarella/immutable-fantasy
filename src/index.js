8d6800ea01cdb80e0685f1dbc5963950a6758ab7
Otto Nascarella <ottoln@gmail.com>
2017-05-13 00:25:17 -0300

    working on range

--- src/index.js
+++ src/index.js
@@ -2,21 +2,48 @@ import FL from '../node_modules/fantasy-land';
 
 const ticketToFantasyLand = (Immutable) => {
 
-  let {Collection} = Immutable;
+  let {Collection, List, Map, Seq} = Immutable;
 
+  Collection.prototype[FL.equals] = Collection.prototype.equals;
+  Collection.prototype[FL.concat] = Collection.prototype.concat;
   Collection.prototype[FL.map] = Collection.prototype.map;
+  Collection.prototype[FL.reduce] = Collection.prototype.reduce;
 
   Collection.prototype.chain = function (fn) {
     return this.flatten(true).map(fn);
   };
 
-  Collection.prototype[FL.chain] = Collection.prototype.chain;
-
-  Collection.prototype[FL.reduce] = Collection.prototype.reduce;
-
   Collection.prototype[FL.ap] = function (other) {
     return this.map(f => other.map(v => f(v))).flatten();
   };
+
+  //
+  // LIST
+  //
+  List[FL.of] = (...args) => List.of(...args);
+  List[FL.empty] = () => List.of();
+  List.prototype[FL.empty] = () => List.of();
+
+  //
+  // MAP
+  //
+  Map[FL.of] = (...args) => Map.of(...args);
+  Map[FL.empty] = () => Map.of();
+  // Map.prototype[FL.empty] = () => Map.of();
+
+  //
+  // SEQ
+  //
+  Seq[FL.of] = (...args) => Seq.of(...args);
+  Seq[FL.empty] = () => Seq.of();
+  Seq.prototype[FL.empty] = () => Seq.of();
+
+  //
+  // RANGE
+  //
+  // Range[FL.of] = (...args) => Seq.of(...args);
+  // Range[FL.empty] = () => Seq.of();
+  // Range.prototype[FL.empty] = () => Seq.of();
 
   return Immutable;
 
--- test/index.spec.js
+++ test/index.spec.js
@@ -2,17 +2,19 @@
 
 import chai from 'chai';
 import ramda from 'ramda';
-import sanctuary from 'sanctuary';
+import sanct from 'sanctuary';
 import Immutable from 'immutable';
 import toFantasyLand from '../lib/immutable-fantasy.js';
 
 const ImmutableFL = toFantasyLand(Immutable);
+const sanctuary = sanct.create({checkTypes: false, env: []});
 
 const {List} = ImmutableFL;
 
 chai.expect();
 
 const expect = chai.expect;
+
 
 describe('List', () => {
 
@@ -53,9 +55,21 @@ describe('List', () => {
 
   });
 
+  it('should be reduceble by sanctuary', () => {
+
+    const list = List([1, 2, 3, 4]);
+    // increase using Immutable API
+    const immutableRes = list.reduce(ramda.add, 1);
+    // increase using Sanctuary
+    const sancRes = sanctuary.reduce(sanctuary.add, 1, list);
+
+    expect(immutableRes).to.equal(sancRes);
+
+  });
+
   it('should be chainable by ramda', () => {
 
-    const ll = List().push(List([1, 2])).push(List([3, 4]));
+    const ll = List.of(List.of(1, 2), List.of(3, 4));
 
     // increase using Immutable API
     const immutableRes = ll.flatten().map(ramda.add(1));
@@ -63,6 +77,19 @@ describe('List', () => {
     const ramdaRes = ramda.chain(ramda.add(1), ll);
 
     expect(ramdaRes.equals(immutableRes)).to.be.true;
+
+  });
+
+  it('should be chainable by sanctuary', () => {
+
+    const ll = List.of(List.of(1, 2), List.of(3, 4));
+
+    // increase using Immutable API
+    const immutableRes = ll.flatten().map(ramda.add(1));
+    // increase using Sanctuary
+    const sancRes = sanctuary.chain(sanctuary.add(1), ll);
+
+    expect(sancRes.equals(immutableRes)).to.be.true;
 
   });
 
@@ -76,6 +103,44 @@ describe('List', () => {
     const ramdaRes = ramda.filter(pred, l);
 
     expect(ramdaRes.equals(immutableRes)).to.be.true;
+
+  });
+
+  it('should be filterable by sanctuary', () => {
+
+    const l = List.of(1, 2, 3, 4);
+
+    const pred = x => x % 2 === 0;
+    const immutableRes = l.filter(pred);
+    const sancRes = sanctuary.filter(pred, l);
+
+    expect(sancRes.equals(immutableRes)).to.be.true;
+
+  });
+
+});
+
+describe('Seq', () => {
+
+  it('should take 5 from infinite range and reduce it with add using ramda', () => {
+
+    const range = ImmutableFL.Range();
+
+    let resImmutable = range.take(5).reduce(ramda.add);
+    let ramdaRes = ramda.reduce(ramda.add, 0, ramda.take(5, range));
+
+    expect(ramdaRes).to.equal(resImmutable);
+
+  });
+
+  it('should take 5 from infinite range and reduce it with add using sanctuary', () => {
+
+    const range = ImmutableFL.Range();
+
+    let resImmutable = range.take(5).reduce(ramda.add);
+    let resSanc = sanctuary.reduce(sanctuary.add, 0, sanctuary.take(5, range));
+
+    expect(resSanc).to.equal(resImmutable);
 
   });
 
