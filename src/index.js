62ff5a354ec6fa68ddfa1938489f1294ce4f6e64
Otto Nascarella <ottoln@gmail.com>
2017-05-15 17:19:11 -0300

    added other types

--- src/index.js
+++ src/index.js
@@ -2,51 +2,32 @@ import FL from '../node_modules/fantasy-land';
 
 const ticketToFantasyLand = (Immutable) => {
 
-  let {Collection, List, Map, Seq} = Immutable;
+  let {Collection, Seq} = Immutable;
 
-  Collection.prototype[FL.equals] = Collection.prototype.equals;
-  Collection.prototype[FL.concat] = Collection.prototype.concat;
-  Collection.prototype[FL.map] = Collection.prototype.map;
-  Collection.prototype[FL.reduce] = Collection.prototype.reduce;
+  // Extends main collection types from which others derive
+  [Collection, Seq].forEach(({prototype}) => {
 
-  Collection.prototype.chain = function (fn) {
-    return this.flatten(true).map(fn);
-  };
+    prototype[FL.equals] = prototype.equals;
+    prototype[FL.concat] = prototype.concat;
+    prototype[FL.map] = prototype.map;
+    prototype[FL.reduce] = prototype.reduce;
 
-  Collection.prototype[FL.ap] = function (other) {
-    return this.map(f => other.map(v => f(v))).flatten();
-  };
+    prototype.chain = prototype[FL.chain] = function (fn) {
+      return this.flatten(true).map(fn);
+    };
 
-  //
-  // LIST
-  //
-  List[FL.of] = (...args) => List.of(...args);
-  List[FL.empty] = () => List.of();
-  List.prototype[FL.empty] = () => List.of();
+    prototype.ap = prototype[FL.ap] = function (other) {
+      return this.map(f => other.map(v => f(v))).flatten();
+    };
+  });
 
-  //
-  // MAP
-  //
-  Map[FL.of] = (...args) => Map.of(...args);
-  Map[FL.empty] = () => Map.of();
-  // Map.prototype[FL.empty] = () => Map.of();
-
-  //
-  // SEQ
-  //
-  Seq[FL.of] = (...args) => Seq.of(...args);
-  Seq[FL.empty] = () => Seq.of();
-  Seq.prototype[FL.empty] = () => Seq.of();
-
-  //
-  // RANGE
-  //
-  // Range[FL.of] = (...args) => Seq.of(...args);
-  // Range[FL.empty] = () => Seq.of();
-  // Range.prototype[FL.empty] = () => Seq.of();
+  ['Stack', 'Record', 'Set', 'OrderedSet', 'List', 'Map', 'OrderedMap'].forEach(type => {
+    Immutable[type][FL.of] = (...args) => Immutable[type].of(...args);
+    Immutable[type][FL.empty] = () => Immutable[type].of();
+    Immutable[type].prototype[FL.empty] = () => Immutable[type].of();
+  });
 
   return Immutable;
-
 };
 
 export default ticketToFantasyLand;
